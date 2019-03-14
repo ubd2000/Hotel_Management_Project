1ff9963a57da0372b72eb2495a4499071dae0934
hoguma <bnbn2400@naver.com>
2019-03-14 10:16:23 +0900

    커밋 제목: HotelBooking의 회원파트 기능 (한 줄 공백) - 회원가입 - 로그인 - 회원정보수정 - 회원탈퇴

--- Member.java
+++ Member.java
@@ -1,5 +1,60 @@
 package kr.or.bit.hotel;
+import java.util.ArrayList;
 
+/**
+ * @author os
+ *
+ */
 public class Member {
+	private Reservation Reservation;
+	private ArrayList<Record[]> Record;
+	private String id; // 추가
+	private String password;
+	private String name;
+	private String birthday;
+	private String phoneNumber;
+	
+	public Member() {
+		Record = new ArrayList<Record[]>();
+		Reservation = new Reservation();
+	}
+	public Member(String name, String password , String phoneNumber , String birthday) {
+		this.name = name;
+		this.password = password;
+		this.phoneNumber = phoneNumber;
+		this.birthday = birthday;
 
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
 }
