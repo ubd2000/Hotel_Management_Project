9e2d7d70248b02db6630b818b66fcb0e29e76fb5
김동민 <ubd20@naver.com>
2019-03-14 10:38:38 +0900

    Version 2.0
    회원관련 코드 완료.
    예약 작업 중.

--- HotelBooking.java
+++ HotelBooking.java
@@ -20,39 +20,93 @@ public class HotelBooking {
 		sc = new Scanner(System.in);
 		hotel = new SmallHotel();
 	}
-	public void getRoomInfo() { // 객실정보보기
-
+	private void getRoomInfo() { // 객실정보보기
+		for (int i = 0; i < hotel.getRoomInfos().length; i++) {
+			String kitchen = "";
+			if (hotel.getRoomInfos()[i].isKitchen()) {
+				kitchen = "유";
+			} else {
+				kitchen = "무";
+			}
+			System.out.println("  [방이름] : " + hotel.getRoomInfos()[i].getRoomName() + "룸  " + "  [가격] : "
+					+ hotel.getRoomPrices()[i] + "원  " + "  [기본인원] : "
+					+ hotel.getRoomInfos()[i].getDefaultNumberPeople() + "명  " + "  [최대인원] : "
+					+ hotel.getRoomInfos()[i].getMaxNumberPeople() + "명  " + "  [화장실] : "
+					+ hotel.getRoomInfos()[i].getNumberBathroom() + "개  " + "  [침대] : "
+					+ hotel.getRoomInfos()[i].getNumberBed() + "개  " + "  [주방] : " + kitchen);
+		}
 	}
 
 	public void reserveRoom() { // 객실예약
-
+		if (memberLoggedIn.getReservation() != null) {
+			System.out.println("이미 예약함");
+			return;
+		}
+		Reservation r = new Reservation();
+		setDate(r);
+		setRoom(r);
+		setNumberPeople(r);
+		setService(r);
+		memberLoggedIn.setReservation(r);
 	}
 
-	public void setNumberPeople() { // 인원설정
-
+	private void setDate(Reservation r) { // 날짜 선택
+		System.out.println("체크인 날짜를 입력해주세요.");
+		String checkIn = sc.nextLine();
+		System.out.println("체크아웃 날짜를 입력해주세요.");
+		String checkOut = sc.nextLine();
+		HotelDate checkInDate = new CheckInDate(2019, 12, 21); // String에서 parse
+		HotelDate checkOutDate = new CheckOutDate(2019, 12, 23);
+		r.setDateCheckIn(checkInDate);
+		r.setDateCheckOut(checkOutDate);
 	}
 
-	public void setRoom() { // 객실 선택
-
+	private void setRoom(Reservation r) { // 객실 선택
+		for (List<Room> floor : hotel.getRooms()) {
+			for (Room room : floor) {
+				if (!(체크인 날짜가 끼어있는 경우 || 체크아웃 날짜가 끼어있는 경우 || 사이에 끼어있는 경우 || 더 범위가 넓은 경우)) {
+					방 번호와 정보를 보여준다
+				}
+			}
+		}
+		
+		System.out.println("예약할 방 번호를 입력해주세요");
+		String roomNumber = sc.nextLine(); // 202호
+		r.setRoom(room);
 	}
 
-	public void setDate() { // 날짜 선택
-
+	private void setNumberPeople(Reservation r) { // 인원설정
+		System.out.println("숙박할 인원을 입력해주세요.");
+		System.out.println("선택하신 방의 기본 인원은 {0}명, 최대 인원은 {1}명입니다.");
+		System.out.println("인원 추가 시 X원이 추가됩니다.");
+		String numberPeople = sc.nextLine();
+		r.setNumberPeople(numberPeople);
 	}
 
-	public void setService() { // 부가서비스 선택
-
+	private void setService(Reservation r) { // 부가서비스 선택
+		System.out.println("부가 서비스를 선택해주세요.");
+		System.out.println("서비스에는 {0}, {1}이 있습니다.");
+		System.out.println("{0}은 X원, {1}은 Y원입니다.");
+		String service = sc.nextLine();
+		r.setService(service);
 	}
 
 	public void getReservation() { // 예약 확인
-
+		System.out.println(memberLoggedIn.getReservation());
 	}
 
 	public void cancelReservation() { // 예약 취소
-
+		/*
+		 * Member - reservation 삭제
+		 * 예약됐던 룸 예약 정보 삭제
+		 */
 	}
 
 	public void changeReservation() { // 예약 변경
+		/*
+		 * reserveRoom();
+		 * cancelReservation();
+		 */
 
 	}
 
