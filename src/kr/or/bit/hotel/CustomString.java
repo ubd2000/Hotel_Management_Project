b94527c6f480d188c4c228a0c141864feb532b55
DarbyJ <jony8674@gmail.com>
2019-03-15 17:02:10 +0900

    // 지훈  조식,테라피 변수설정  호텔생성 설명 추가

--- src/kr/or/bit/hotel/CustomString.java
+++ src/kr/or/bit/hotel/CustomString.java
@@ -7,10 +7,14 @@ import java.time.format.DateTimeFormatter;
 public class CustomString {
 	public static final String PATH_DIRECTORY = "C:\\Temp\\Hotel\\";
 	public static final String PATH_HOTEL = "C:\\Temp\\Hotel\\hotel.info";
+	public static final String breakfast = "조식";
+	public static final String therapy = "전신테라피";
+	
 	public static final String PATH_RECORD_DIRECTORY(LocalDate date) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 		return "C:\\Temp\\Hotel\\" + date.format(formatter);
 	}
+	
 	
 	public static final String PATH_RECORD(LocalDate date, String check) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
